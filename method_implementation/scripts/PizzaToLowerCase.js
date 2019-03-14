b99f10860b97f2ec15b7abb45107f50ba5e89583
Muhammad Harits Syaifulloh <muhammadharits@icloud.com>
2019-03-14 09:27:41 +0700

    Method and method mapping implementation

---
+++ method_implementation/scripts/PizzaToLowerCase.js
@@ -0,0 +1,11 @@
+/*
+* @Author: Muhammad Harits Syaifulloh
+* @Date:   2019-03-13 14:30:08
+* @Last Modified by:   Muhammad Harits Syaifulloh
+* @Last Modified time: 2019-03-13 16:17:47
+*/
+
+function pizzaToLowerCase(ctx) {
+	ctx.context.pizza = ctx.context.pizza.toLowerCase();
+	return ctx;
+}
